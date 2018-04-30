b75f443e5c0dc7f9550ab8d306510f900b438766
Tom Barwick <tom_barwick@hotmail.com>
2018-04-30 14:52:45 +0100

    Ads.js file added. Starts adblock detection testing

---
+++ static/ads.js
@@ -0,0 +1,8 @@
+/*
+  Most adblockers will block any file from being loaded called 'ads.js'
+*/
+
+const fakeAd = document.createElement('div');
+fakeAd.id = 'e091767e-7e41-4d4d-9228-2949cda46378';
+fakeAd.style.display = 'none';
+document.body.appendChild(fakeAd);
