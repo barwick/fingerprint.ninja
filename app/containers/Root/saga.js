b04d08c16ccb4e31836eb41f3ea752b822ca1c58
Tom Barwick <tom_barwick@hotmail.com>
2018-05-21 16:21:20 +0100

    Fix conflicting prettier and eslint ruling

--- app/containers/Root/saga.js
+++ app/containers/Root/saga.js
@@ -2,6 +2,6 @@ import { fork } from 'redux-saga/effects';
 
 import { publishFingerprintWatcher } from 'app/containers/Fingerprint/saga';
 
-export default function*() {
+export default function* () {
   yield fork(publishFingerprintWatcher);
 }
