5c0a43b099730cbacb2e0375e549eeefc81f3bfa
Tom Barwick <tom_barwick@hotmail.com>
2018-05-21 16:11:35 +0100

    Adds redux + sagas

---
+++ app/containers/Root/saga.js
@@ -0,0 +1,7 @@
+import { fork } from 'redux-saga/effects';
+
+import { publishFingerprintWatcher } from 'app/containers/Fingerprint/saga';
+
+export default function*() {
+  yield fork(publishFingerprintWatcher);
+}
--- app/index.js
+++ app/index.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
 
-import Root from 'app/containers/Root';
+import styled from 'styled-components';
 
-ReactDOM.render(<Root />, document.getElementById('app'));
+import RootComponent from 'app/containers/Root/component';
+import sagas from 'app/containers/Root/saga';
+import reducer from 'app/containers/Root/reducer';
+
+import './style'; // Global CSS Styling
+
+/* Initialise app state */
+const logger = createLogger();
+const reduxSaga = createSagaMiddleware();
+
+const isDev = Boolean(process.env.NODE_ENV !== 'prod');
+const middleware = isDev ? [logger, reduxSaga] : [reduxSaga];
+
+const store = createStore(reducer, applyMiddleware(...middleware));
+
+reduxSaga.run(sagas);
+/* End initialise app state */
+
+const RootContainer = styled.div`
+  height: 100%;
+`;
+
+ReactDOM.render(
+  <Provider store={store}>
+    <RootContainer>
+      <RootComponent />
+    </RootContainer>
+  </Provider>,
+  document.getElementById('app'),
+);
