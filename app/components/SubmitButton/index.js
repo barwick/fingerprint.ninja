a85c6f8cee612e448cc7f9f4fbea65dfdb13e9de
Tom Barwick <tom_barwick@hotmail.com>
2018-05-31 00:13:30 +0100

    Adds styling to button states (e.g. disabled) and adds loading spinner

--- app/components/SubmitButton/index.js
+++ app/components/SubmitButton/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+import loadingSpinner from 'static/loading.svg';
+
 const Button = styled.button`
   font-family: 'Montserrat', sans-serif;
   background-color: rgba(0, 0, 0, 0);
@@ -9,21 +11,42 @@ const Button = styled.button`
   height: 50px;
   border: 2px solid var(--col-font);
   border-radius: 5px;
-  cursor: pointer;
+  cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
   outline: none;
 
-  &:hover {
-    color: var(--col-bg);
-    border-color: var(--col-font);
-    background-color: #666;
-  }
+  ${props =>
+    !props.disabled &&
+    `
+    &:hover {
+      color: var(--col-bg);
+      border-color: var(--col-font);
+      background-color: #666;
+    }
+  `};
 `;
 
-const SubmitButton = ({ onSubmit, text }) => <Button onClick={onSubmit}>{text}</Button>;
+const Spinner = styled.img`
+  margin-top: 5px;
+`;
+
+const SubmitButton = ({
+  onSubmit, text, loading, disabledProp,
+}) => (
+  <Button onClick={onSubmit} disabled={disabledProp}>
+    {loading ? <Spinner alt="" src={loadingSpinner} /> : text}
+  </Button>
+);
 
 SubmitButton.propTypes = {
   onSubmit: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
+  loading: PropTypes.bool,
+  disabledProp: PropTypes.bool,
+};
+
+SubmitButton.defaultProps = {
+  loading: false,
+  disabledProp: false,
 };
 
 export default SubmitButton;
