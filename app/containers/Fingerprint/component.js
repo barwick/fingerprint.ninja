89d98e6b4cdc78df644ec274ea5ceb26280c7996
Tom Barwick <tom_barwick@hotmail.com>
2018-06-07 08:30:29 +0100

    Adds modal to frontend. Adds a bit of info for users

--- app/containers/Fingerprint/component.js
+++ app/containers/Fingerprint/component.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import SubmitButton from 'app/components/SubmitButton';
+import Modal from 'app/components/Modal';
 
 const Container = styled.div`
   display: flex;
@@ -10,14 +11,40 @@ const Container = styled.div`
   align-items: center;
 `;
 
-// <div>
-//   {Object.values(ProbesLib).map(probe => <ProbeItem key={probe.name} probe={probe} />)}
-// </div>
+const Subtitle = styled.p`
+  text-align: center;
+  font-weight: bold;
+  font-style: italic;
+`;
+
+const Link = styled.a`
+  color: var(--font-col);
+`;
 
 const getText = (error, success) => {
   if (error) return 'Error. Try again...';
   return success ? 'Thank you' : 'Submit your fingerprint';
 };
+
+const links = {
+  'Wikipedia - Device fingerprint': 'https://en.wikipedia.org/wiki/Device_fingerprint',
+  'Browser Leaks': 'https://browserleaks.com/',
+  Panopticlick: 'https://panopticlick.eff.org/',
+  'Am I Unique?': 'https://amiunique.org/',
+};
+
+const generateExternalLinks = () => (
+  <p style={{ textAlign: 'center' }}>
+    {Object.keys(links).map(name => (
+      <React.Fragment key={`ext-${name}-fragment`}>
+        <Link key={`ext-${name}`} href={links[name]} target="_blank" rel="noopener noreferrer">
+          {name}
+        </Link>
+        <br />
+      </React.Fragment>
+    ))}
+  </p>
+);
 
 const Probes = ({
   publishFingerprint, loading, success, error,
@@ -29,6 +56,28 @@ const Probes = ({
       loading={loading.length > 0}
       disabledProp={loading.length > 0 || success}
     />
+    <Modal buttonText={"What's it all about?"}>
+      <Subtitle>Hi, my name is Tom</Subtitle>
+      <p>
+        I&apos;m a 4th year Computing student at Imperial College London. I&apos;m using this site
+        to collect data for my masters project, using the results for analysis and conclusions about
+        the current state of device fingerprinting.
+      </p>
+      <Subtitle>What does that button actually do?</Subtitle>
+      <p>
+        Once you click the &apos;Submit your fingerprint&apos; button, a library of scripts are run
+        which collect information about your browser. This data is securely sent to an encrypted
+        database so I can analyse it later!
+      </p>
+      <Subtitle>What now?</Subtitle>
+      <p>
+        Hopefully I will be able to expand the site in the future to provide a more user friendly
+        experience, including a breakdown of your fingerprint, an expanded library of probes and
+        more content to educate.
+      </p>
+      <p>Here are a few links if you want to read more about what device fingerprinting is:</p>
+      {generateExternalLinks()}
+    </Modal>
   </Container>
 );
 
